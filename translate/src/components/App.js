1a231996ab491b1639e4156f3e5627e9a2e7ab6c
florian lahitte <lahitte.florian@gmail.com>
2019-06-25 16:15:22 -0400

    Add context part 2

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,32 +1,20 @@
 import React from 'react';
 import UserCreate from '../components/UserCreate'
-import LanguageContext from '../contexts/LanguageContext'
+import {LanguageStore} from '../contexts/LanguageContext'
 import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from './LanguageSelect';
 
 class App extends React.Component{
-
-    state = {
-        language: 'english'
-    };
-
-    onLanguageChange = (language)=>{
-        this.setState({language});
-    }
-
     render(){
         return (
             <div className="ui container">
-                <div>
-                    Select a language:
-                    <i className="flag us" onClick={()=> this.onLanguageChange('english')}/>
-                    <i className="flag es" onClick={()=> this.onLanguageChange('spain')}/>
-                </div>
-                <ColorContext.Provider value={"red"}>
-                    <LanguageContext.Provider value={this.state.language}>
+                <LanguageStore>
+                    <LanguageSelector/>
+                    
+                    <ColorContext.Provider value={"red"}>
                         <UserCreate />
-                    </LanguageContext.Provider>
-                </ColorContext.Provider>
-                
+                    </ColorContext.Provider>
+                </LanguageStore>
             </div>
         )
     }
